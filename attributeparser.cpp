7cd69f80a49d4bb208f3b5e9aeb4a5a86926553d
M.Venkata Shanmukha Viswanadh <shanmukhamarthi@gmail.com>
2026-08-26 15:36:09 +0530

    Update attributeparser.cpp

--- attributeparser.cpp
+++ attributeparser.cpp
@@ -4,57 +4,75 @@
 #include <iostream>
 #include <algorithm>
 using namespace std;
+struct Element {
+	string tagname;
+	string attributename;
+	string value;
+};
+string parser(string command, Element e) {
+	int i=0;
+	int s=0;
+	string tagname;
+	string attributename;
+	string value;
+	bool completion=false;
+	if(command[0]=='<' && command[command.length()-1]=='>') {
+		for(int s=0; s<command.length(); s++) {
+			if(command[s]=='/') {
+				completion=true;
+			}
 
-string parser(string command){
-    int i=0;
-     string tagname;
-        string attributename;
-        string value;
-    if(command[0]=='<' && command[command.length()-1]=='>'){
-        i=1;
-        while(command[i]!=' '){
-            tagname+=command[i];
-            i++;
-            
-        }
-        i++;
-        while(command[i]!=' '){
-            attributename+=command[i];
-            i++;
-        }
-         
-         while(command[i]!='='){
-             i++;
-         }
-         i++;
-         while(command[i]!='"'){
-             i++;
-         }
-         i++;
-         while(command[i]!='"'){
-             value+=command[i];
-             i++;
-         }
-        
-        
-    
-     cout<<tagname<<" "<<attributename<<" "<<value;
-    }
-    
-    return tagname,attributename,value;
+
+		}
+		if(completion!=false){
+
+		i=1;
+		while(command[i]!=' ') {
+			tagname+=command[i];
+			i++;
+
+		}
+		i++;
+		while(command[i]!=' ') {
+			attributename+=command[i];
+			i++;
+		}
+
+		while(command[i]!='=') {
+			i++;
+		}
+		i++;
+		while(command[i]!='"') {
+			i++;
+		}
+		i++;
+		while(command[i]!='"') {
+			value+=command[i];
+			i++;
+		}
+
+
+
+		cout<<tagname<<" "<<attributename<<" "<<value;
+	}
+	}
+	e.tagname=tagname;
+	e.attributename=attributename;
+	e.value=value;
+	return e;
+
 }
-int main(){
-    int n;
-    int q;
-    string commands[n];
-    cin>>n>>q;
-    for(int i=0; i< n ; i++){
-    string command;
-    getline(cin,command);
-    commands[i]+=parser(command);
-    commands+="%";
-    
-    }
-    
+int main() {
+	int n;
+	int q;
+	cin>>n>>q;
+	struct Element e[n];
+	for(int i=0; i< n ; i++) {
+		string command;
+		getline(cin,command);
+		parser(command,e[i]);
+		
+
+	}
+
 }
-im getting theree
