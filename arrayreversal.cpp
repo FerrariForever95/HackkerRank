367676bc9c6efa93e558d535003f7058a6d1e2d5
M.Venkata Shanmukha Viswanadh <shanmukhamarthi@gmail.com>
2026-08-26 11:17:20 +0530

    Create arrayreversal.cpp

---
+++ arrayreversal.cpp
@@ -0,0 +1,24 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    int size;
+    cin>>size;
+    int temp;
+    int arr[size];
+    for(int i=0;i<size;i++){
+        cin>>arr[i];
+        
+    }
+    reverse(arr,arr+size);
+    for(int i=0;i<size;i++){
+        cout<<arr[i]<<" ";
+    }
+    return 0;
+    
+}
