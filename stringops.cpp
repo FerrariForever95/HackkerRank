57c8e1ff4c43a9d330da5163e417c49c137d7caf
M.Venkata Shanmukha Viswanadh <shanmukhamarthi@gmail.com>
2026-08-27 08:43:18 +0530

    Create stringops.cpp

---
+++ stringops.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+	string a,b;
+    char t;
+    cin>>a>>b;
+    cout<<a.length()<<" "<<b.length()<<"\n"<<a+b<<"\n";
+    t=a[0];
+    a[0]=b[0];
+    b[0]=t;
+    cout<<a<<" "<<b;
+    return 0;
+}
