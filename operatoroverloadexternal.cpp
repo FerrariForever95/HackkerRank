4088d1c12f0de4385aea43b487d44751afc02a47
M.Venkata Shanmukha Viswanadh <shanmukhamarthi@gmail.com>
2026-08-28 11:31:11 +0530

    Update operatoroverloadexternal.cpp

--- operatoroverloadexternal.cpp
+++ operatoroverloadexternal.cpp
@@ -46,7 +46,7 @@ ostream& operator<<(ostream& out, Complex& c)
 //Overload operators + and << for the class complex
 //+ should add two complex numbers as (a+ib) + (c+id) = (a+c) + i(b+d)
 //<< should print a complex number in the format "a+ib"
-complex operator
+
 int main()
 {
     Complex x,y;
