077ce9bbee697f0c1e4c703f9b8de7fed8023001
M.Venkata Shanmukha Viswanadh <shanmukhamarthi@gmail.com>
2026-08-28 10:58:44 +0530

    Create virtualfuntions.cpp

---
+++ virtualfuntions.cpp
@@ -0,0 +1,70 @@
+class Person {
+public:
+    string name;
+    int age;
+
+    virtual void getdata() {
+        cin >> name >> age;
+    }
+
+    virtual void putdata() {
+        cout << name << " " << age << "\n";
+    }
+};
+
+class Professor : public Person {
+public:
+    int publications;
+    int cur_id;
+
+    static int id;
+
+    Professor() {
+        cur_id = ++id;
+    }
+
+    void getdata() {
+        cin >> name >> age >> publications;
+    }
+
+    void putdata() {
+        cout << name << " " << age << " "
+             << publications << " " << cur_id << "\n";
+    }
+};
+
+int Professor::id = 0;
+
+
+class Student : public Person {
+public:
+    int marks[6];
+    int cur_id;
+
+    static int id;
+
+    Student() {
+        cur_id = ++id;
+    }
+
+    void getdata() {
+        cin >> name >> age;
+
+        for (int i = 0; i < 6; i++) {
+            cin >> marks[i];
+        }
+    }
+
+    void putdata() {
+        int sum = 0;
+
+        for (int i = 0; i < 6; i++) {
+            sum += marks[i];
+        }
+
+        cout << name << " " << age << " "
+             << sum << " " << cur_id << "\n";
+    }
+};
+
+int Student::id = 0;
